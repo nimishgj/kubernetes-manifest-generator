a27b66fd53836c04b098fbda7379bba58d37e285
nimishgj <nimishgj444@gmail.com>
2025-07-10 07:31:54 +0530

    Updates

--- _app/immutable/chunks/Dmgqhhoc.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_q30j6c)==null?void 0:s.base)??"/kubernetes-manifest-generator";var e;const a=((e=globalThis.__sveltekit_q30j6c)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.DxrnEI-K.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/bC5k1eQx.js";export{o as load_css,r as start};
